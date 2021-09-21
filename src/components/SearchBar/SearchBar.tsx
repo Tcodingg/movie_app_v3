8602e9e6d34e6b35f97df146cfda72d524aa53da
tcodingg <Princetlive@yahoo.com>
2021-09-21 17:30:20 -0400

    add onkeydown and onchange functionality to the search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./SearchBar.css";
 
 const SearchBar: React.FC = () => {
+  const [input, setInput] = useState<string>("");
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInput(e.currentTarget.value);
+  };
+
+  const handleKeydown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter" && input !== "") {
+      console.log(input);
+    }
+  };
   return (
     <section className="search-bar">
-      <input type="text" />
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="Search..."
+        value={input}
+        onKeyDown={handleKeydown}
+      />
     </section>
   );
 };
